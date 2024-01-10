6695d8df8766ad8677c33b62e73e702ef94d4a3a
Srivignesh-2 <sc5908@srmist.edu.in>
2024-01-10 13:04:12 +0530

    Version

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+Jobs = [
+    {
+        "id": 1,
+        "job_profile": "Python Developer",
+        "location": "Chennai, India",
+        "salary": "7,50,000"
+    },
+    {
+        "id": 2,
+        "job_profile": "Full Stack Developer",
+        "location": "Banglore, India",
+        "salary": "10,00,000"
+    },
+    {
+        "id": 3,
+        "job_profile": "Java Developer",
+        "location": "Coimbatore, India",
+        "salary": "8,50,000"
+    }
+]
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', jobs=Jobs)
+
+
+@app.route('/api/jobs')
+def all_jobs():
+    return jsonify(Jobs)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
+
